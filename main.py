26fc813855d8de0aacd1bcfe09ac9f67a534df95
Lori Liu <lyl2505@rit.edu>
2021-12-01 19:55:43 -0500

    shame list exists but ugly

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 intents = discord.Intents.default()
 intents.members = True
 client = discord.Client(intents = intents)
-
 bot = commands.Bot(command_prefix='!', intents = intents)
 
 @bot.event
@@ -19,13 +18,27 @@ async def on_ready():
     print(f'{client.user} has connected to Discord')
 
 @bot.command(name='shame', help='Prints out the members of the shame list')
-async def on_command(ctx):
-    role = get(ctx.guild.roles, name='Shame')
-    members = await ctx.guild.fetch_members().flatten()   
-    await ctx.send(members)
-    """ for member in ctx.message.guild.members:
-        await ctx.send(member.name)
-        if role in member.roles:
-            await ctx.send(member.name) """
+async def on_command(ctx, *args):
+    if len(args) == 0:
+        role = get(ctx.guild.roles, name='Shame Listed')
+        members = await ctx.guild.fetch_members().flatten()  
+        if len(members) == 0:
+            await ctx.send("Wow, there are no members to shame!") 
+        for member in members:
+            if role in member.roles:
+                await ctx.send(member.name)
+    elif args[0] == 'init':
+        shame_list_channel = discord.utils.get(ctx.guild.text_channels, name='shame-list')
+        await shame_list_channel.purge() # clears entire shame-list channel
+        members = await ctx.guild.fetch_members().flatten() 
+        role = get(ctx.guild.roles, name='Shame Listed')
+        shame_list = '```The Shame List\n'
+        for member in members:
+            if role in member.roles:
+                shame_list += member.name + '\n'
+        shame_list = shame_list + '```'
+        await shame_list_channel.send(shame_list)
+    else:
+        await ctx.send('Not a valid command')
 
 bot.run(TOKEN)
